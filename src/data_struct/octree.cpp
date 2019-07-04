abd7be4080d8e6567421d4f601b33a0b948f02ef
huangwei <1845739048@qq.com>
2019-07-04 23:18:53 +0800

    2019-7-4-23:18-home

--- src/data_struct/octree.cpp
+++ src/data_struct/octree.cpp
@@ -1,4 +1,5 @@
 #include "octree.h"
+#include <iostream>
 
 namespace curiosity {
     namespace graphics {
@@ -10,7 +11,7 @@ namespace curiosity {
 
     void OcTree::build(float xMin, float xMax, float yMin,
                        float yMax, float zMin, float zMax,
-                       const std::vector<NodeContent> &nodeContents) {
+                       const std::vector<NodeContent *> &nodeContents) {
         // 创建根结点
         root = new TreeNode;
         root->xMin = xMin;
@@ -22,6 +23,48 @@ namespace curiosity {
         // 将剩下来的结点插入
         for (auto content : nodeContents)
             insert(root, content);
+    }
+
+
+    // 插入新的节点
+    void OcTree::insert(TreeNode *node, NodeContent *content) {
+        TreeNode::Region region = node->whichRegion(content);
+        TreeNode **insertPos;
+        switch(region) {
+        case TreeNode::A:
+            insertPos = &(node->a);
+            break;
+        case TreeNode::B:
+            insertPos = &(node->b);
+            break;
+        case TreeNode::C:
+            insertPos = &(node->c);
+            break;
+        case TreeNode::D:
+            insertPos = &(node->d);
+            break;
+        case TreeNode::E:
+            insertPos = &(node->e);
+            break;
+        case TreeNode::F:
+            insertPos = &(node->f);
+            break;
+        case TreeNode::G:
+            insertPos = &(node->g);
+            break;
+        case TreeNode::H:
+            insertPos = &(node->h);
+            break;
+        default:
+            std::cout << __func__ << "Should't run to here" << std::endl;
+        }
+        if (*insertPos) {
+            insert(*insertPos, content);
+        } else {
+            TreeNode *newNode = new TreeNode();
+            newNode->nodeContent = content;
+            *insertPos = newNode;
+        }
     }
 
     }
--- src/data_struct/octree.h
+++ src/data_struct/octree.h
@@ -11,19 +11,84 @@ namespace curiosity {
 
     class OcTree {
     public:
-        typedef struct TreeNode TreeNode;
-        typedef struct NodeContent NodeContent;
+//        typedef struct TreeNode TreeNode;
+//        typedef struct NodeContent NodeContent;
 
         struct NodeContent {
             void *data;
             float x, y, z;
             NodeContent *next;
         };
+
         struct TreeNode {
             TreeNode() {
                 a = b = c = d = e = f = g = h = NULL;
                 nodeContent = NULL;
             }
+
+            enum Region {
+                A, B, C, D, E, F, G, H
+            };
+            Region whichRegion(NodeContent *content) {
+                if (xMax - content->x <= content->x - xMin) {
+                    if (yMax - content->y <= content->y - yMin) {
+                        if (zMax - content->z <= content->z - zMin) {
+                            return A;
+                        } else {
+                            return E;
+                        }
+                    } else {
+                        if (zMax - content->z <= content->z - zMin) {
+                            return B;
+                        } else {
+                            return F;
+                        }
+                    }
+                } else {
+                    if (yMax - content->y <= content->y - yMin) {
+                        if (zMax - content->z <= content->z - zMin) {
+                            return D;
+                        } else {
+                            return H;
+                        }
+                    } else {
+                        if (zMax - content->z <= content->z - zMin) {
+                            return C;
+                        } else {
+                            return G;
+                        }
+                    }
+                }
+            }
+
+            std::vector<float> getNewRegion(Region r) {
+                std::vector<float> ret;
+                switch (r) {
+                case A:
+                    ret.push_back(xMax);
+                    ret.push_back(yMax);
+                    ret.push_back(zMax);
+                    ret.push_back((xMax + xMin)/2.0f);
+                    ret.push_back((yMax + yMin)/2.0f);
+                    ret.push_back((zMax + zMin)/2.0f);
+                    break;
+                case B:
+                    break;
+                case C:
+                    break;
+                case D:
+                    break;
+                case E:
+                    break;
+                case F:
+                    break;
+                case G:
+                    break;
+                case H:
+                    break;
+                }
+            }
+
             TreeNode *a, *b, *c, *d,
                      *e, *f, *g, *h;
             NodeContent *nodeContent;
@@ -36,7 +101,7 @@ namespace curiosity {
         void build(float xMin, float xMax,
                    float yMin, float yMax,
                    float zMin, float zMax,
-                   const std::vector<NodeContent> &nodeContents);
+                   const std::vector<NodeContent *> &nodeContents);
         void insert(TreeNode *root, NodeContent *content);
 
     private:
